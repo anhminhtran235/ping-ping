7cc2cdb473e5dbfa6b0e4cb639cd037ef5869d54
Anh Minh Tran <anhminhtran235@gmail.com>
2020-09-24 16:49:29 -0700

    Set up for deploy

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDb = require('./config/db');
 const sendPing = require('./sendPing');
+const path = require('path');
+
 const app = express();
 
 // Connect Database
@@ -9,15 +11,23 @@ connectDb();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Runnning'));
-
 // Router
 app.use('/api/appUrl', require('./routes/api/appUrl'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
 
 // Sending ping to subscribed urls
-const fiveSeconds = 5 * 1000;
-setInterval(sendPing, fiveSeconds);
+const tenMinutes = 10 * 60 * 1000;
+setInterval(sendPing, tenMinutes);
